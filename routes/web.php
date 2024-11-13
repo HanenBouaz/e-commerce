735adaa12eb3deadecafd9356ab16cc1ac6d105d
Hanen Oumaima <hanen.bouaziz18@gmail.com>
2024-11-13 11:32:36 +0100

    Commit new version

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\CategorieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
